291ac3d5a4a49c5870874bd46b9a7cec4b09fa45
Raymond Kam <rkam@connected.io>
2021-04-05 16:54:02 -0400

    Added Colors to be shared

---
+++ TestLibrary/ConnectedStyle.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+public extension Color {
+    static let connectedGray = Color(red: 0.47, green: 0.47, blue: 0.47, opacity: 1.0) // 777777
+    static let connectedBlue = Color(red: 0, green: 0.45, blue: 0.79, opacity: 1.0)
+    static let connectedTeal = Color(red: 0.133, green: 0.824, blue: 0.698, opacity: 1.0)
+    static let connectedRed = Color(red: 0.992, green: 0.275, blue: 0.282, opacity: 1.0)
+    static let connectedYellow = Color(red: 236/255, green: 178/255, blue: 46/255)
+}
+
