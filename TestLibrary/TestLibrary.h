1868d15b6eaa4a413b1606e34ac40904f3d8d5ff
Raymond Kam <rkam@connected.io>
2021-04-05 16:42:07 -0400

    Initial commit

---
+++ TestLibrary/TestLibrary.h
@@ -0,0 +1,18 @@
+//
+//  TestLibrary.h
+//  TestLibrary
+//
+//  Created by cl-dev on 2021-04-05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibrary.
+FOUNDATION_EXPORT double TestLibraryVersionNumber;
+
+//! Project version string for TestLibrary.
+FOUNDATION_EXPORT const unsigned char TestLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrary/PublicHeader.h>
+
+
